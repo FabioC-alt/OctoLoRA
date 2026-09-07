27ec7d45d93c9123fb7423b62f407c1845a83022
FabioC-alt <fabiociraci41@gmail.com>
2026-09-07 14:49:19 +0200

    Changed Resource Requests

--- scripts/submit_octolora.sh
+++ scripts/submit_octolora.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=OctoLoRAX.out
 #SBATCH --error=OctoLoRAX.err
 #SBATCH --partition=l40s
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 #SBATCH --time=08:00:00
